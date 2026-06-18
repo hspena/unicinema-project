aa88d5e4e213ef7bceb30501598765cf403bfa72
hspena <hadifshohaimi@gmail.com>
2026-06-18 19:17:53 +0800

    move Firebase config to env vars (.env), keep keys out of VCS

--- src/app/config/firebase.ts
+++ src/app/config/firebase.ts
@@ -10,14 +10,14 @@ import { getAuth }       from 'firebase/auth';
 import { getDatabase }   from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAU_UGvHvIQhwgTFLRkT1N9imvT3Zi4Ux0",
-  authDomain: "unicinema-8666b.firebaseapp.com",
-  databaseURL: "https://unicinema-8666b-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "unicinema-8666b",
-  //storageBucket: "unicinema-8666b.firebasestorage.app",
-  messagingSenderId: "463399137672",
-  appId: "1:463399137672:web:1007333c00d3f649d4ca3d",
-  measurementId: "G-0P1W2YH0N9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Recommended Firestore collections:
